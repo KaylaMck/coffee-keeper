1b99ae103d92e155f93005a1e5d02b2c2a939446
kaylamckenzie <kmckenzie82@bethelu.edu>
2024-11-13 15:40:26 -0600

    Completed Register page and CSS

---
+++ src/components/register/RegisterNewUser.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { addNewUser } from "../services/userServices";
+import "./RegisterNewUser.css";
+
+export const RegisterNewUser = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    username: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addNewUser(user).then((createdUser) => {
+      if (createdUser && createdUser.id) {
+        localStorage.setItem(
+          "coffee-user",
+          JSON.stringify({
+            id: createdUser.id,
+            name: createdUser.name,
+            username: createdUser.username,
+          })
+        );
+
+        navigate("/");
+
+        setUser({
+          name: "",
+          email: "",
+          username: "",
+        });
+      }
+    });
+  };
+
+  return (
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <h2>Coffee Keeper</h2>
+        <h3>Sign Up</h3>
+
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            id="name"
+            value={user.name}
+            placeholder="Full Name"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="email"
+            id="email"
+            value={user.email}
+            placeholder="Email"
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            id="username"
+            value={user.username}
+            placeholder="Username"
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div className="button-container">
+          <button type="submit" className="btn-info">
+            Create Account
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
